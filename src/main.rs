11e5dd1b25ffe47984c80a47f883a6f45c5ac541
RudhyDev <rudhy.pereira@gmail.com>
2022-10-22 18:43:10 -0300

    feat: :sparkles: new route Get for health check

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,34 @@ use actix_web::{ get, web, App, HttpServer };
 use serde::{ Serialize, Deserialize };
 use std::sync::Mutex;
 
+struct Appstate {
+    todolist_entries: Mutex<Vec<TodolistEntry>>
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct TodolistEntry {
+    id: i32,
+    date: i64,
+    title: String
+}
+
+#[get("/")]
+async fn index() -> String {
+    "Health check route".to_string()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let app_data = web::Data::new(Appstate{
+        todolist_entries: Mutex::new(vec![])
+    });
+
+    HttpServer::new(move || {
+        App::new()
+        .app_data(app_data.clone())
+        .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
